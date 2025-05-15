aa3b3eac0afc8430bbe01cf6db0f015d87d83726
Toluwani Okunowo <43044740+nowodev@users.noreply.github.com>
2025-05-14 18:20:44 -0700

    Building app layout

---
+++ src/components/Map.jsx
@@ -0,0 +1,12 @@
+import styles from "./Map.module.css";
+
+function Map() {
+  return (
+    <div className={styles.mapContainer}>
+        MAP
+      <div className={styles.map}></div>
+    </div>
+  );
+}
+
+export default Map;
