b1927deea33b0525d56cdef5b02592620708b9ab
Toluwani Okunowo <43044740+nowodev@users.noreply.github.com>
2025-05-14 18:05:30 -0700

    Building the pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import { BrowserRouter } from "react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
