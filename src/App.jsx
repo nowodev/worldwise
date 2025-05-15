a29dd4dd758fd8542e81ad8ca161448231ee913f
Toluwani Okunowo <43044740+nowodev@users.noreply.github.com>
2025-05-15 09:15:13 -0700

    Implementing the Countries List

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
+
 import HomePage from "./pages/HomePage";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
@@ -7,6 +8,7 @@ import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
 
 const BASE_URL = "http://localhost:9000";
 function App() {
@@ -39,7 +41,7 @@ function App() {
         <Route path="/app" element={<AppLayout />}>
           <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
---
+++ src/components/CountryList.jsx
@@ -0,0 +1,32 @@
+import CountryItem from "./CountryItem";
+import Spinner from "./Spinner";
+import styles from "./CountryList.module.css";
+import Message from "./Message";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [
+        ...arr,
+        { id: city.id, country: city.country, emoji: city.emoji },
+      ];
+    else return arr;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem key={country.id} country={country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
