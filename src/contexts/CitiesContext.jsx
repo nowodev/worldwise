e9082fb667bcaaca67c23fa650fe7fecb8f87285
Toluwani Okunowo <43044740+nowodev@users.noreply.github.com>
2025-08-03 20:13:11 -0700

    Consuming the CitiesContext

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -34,4 +34,12 @@ function CitiesProvider({ children }) {
   );
 }
 
-export { CitiesProvider };
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (!context) {
+    throw new Error("useCities must be used within a CitiesProvider");
+  }
+  return context;
+}
+
+export { CitiesProvider, useCities };
